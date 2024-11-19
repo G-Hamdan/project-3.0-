3336573dee5e8e55b3c36e857b1e7a8c7742344e
G-Hamdan <94384827+G-Hamdan@users.noreply.github.com>
2024-11-19 20:50:46 +0100

    final

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const express = require("express");
+const cors = require("cors"); // Import cors
 const app = express();
 const port = 5000;
-const usersRouter = require("./routes/users.js")
-const sqlite3 = require("sqlite3").verbose()
+const usersRouter = require("./routes/users.js");
+const sqlite3 = require("sqlite3").verbose();
 
+// Enable CORS for all origins (for development purposes)
+app.use(cors());
+
+// If you want to restrict to your frontend origin, you can specify it:
+// app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use(express.json());
-app.use("/api/", usersRouter)
+app.use("/api/", usersRouter);
 
 app.get("/", (req, res) => {
 	res.json({
 		msg: "welcome to my users api ",
-	})
-})
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+	});
 });
 
-
-
-
-
+app.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
+});
